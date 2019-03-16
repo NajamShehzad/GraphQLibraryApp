0684de586f77e9abe98e95445e37a3de4ab5cb21
NajamShehzad <najambutt195@gmail.com>
2019-03-16 21:36:24 +0500

    Book Schema

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
-app.use('/graphql',graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({
+
+}));
 
 
 app.listen(4000, () => {
