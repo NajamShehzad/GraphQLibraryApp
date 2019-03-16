f86c5e15e283630382fe6e6bbed9e8a28cb4c053
NajamShehzad <najambutt195@gmail.com>
2019-03-16 15:31:56 +0500

    Initilize GraphQL With Express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql',graphqlHTTP({}));
+
+
+app.listen(4000, () => {
+    console.log("Server Running on Port 4000")
+});
